42aa87d8de601ae542cf52e8942833464e72716f
Tomasz Pazdziora <tomekpazdziora@gmail.com>
2023-03-27 16:27:05 +0200

    test push

--- Core/Src/driver.c
+++ Core/Src/driver.c
@@ -7,10 +7,11 @@
 extern void ExStartDriveTask(void const * argument)
 {
   /* USER CODE BEGIN StartDriveTask */
+  int a = 5;
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1000);
+    osDelay(2000);
     HAL_GPIO_WritePin(LED_CTR_GPIO_Port, LED_CTR_Pin, GPIO_PIN_RESET);
     osDelay(2000);
   }
