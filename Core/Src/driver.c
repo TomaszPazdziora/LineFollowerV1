438313d9917d375a396b9f8d54dc1e72f899cd40
Tomasz Pazdziora <tomekpazdziora@gmail.com>
2023-04-14 19:39:13 +0200

    add motor driver control finctions

--- Core/Src/driver.c
+++ Core/Src/driver.c
@@ -9,16 +9,35 @@ int readings[SENSOR_NUMBER];
 void clearReadings();
 void measure();
 
+void turnLeft1motor();
+void turnRight1motor();
+
+void turnLeft2motor();
+void turnRight2motor();
+
 /* ---- TASK IMPLEMENTATION ---- */
 
 extern void ExStartDriveTask(void const * argument) {
   /* USER CODE BEGIN StartDriveTask */
+  turnRight1motor();
+  turnRight2motor();
   /* Infinite loop */
   for(;;)
   {
-    osDelay(SENSOR_DELAY);
-    measure();
-    osDelay(SENSOR_DELAY);
+	for(int i = 19; i > 0; i--) {
+		// 19 = 100 % pwm duty
+		// 0 = 0% pwm duty
+		// duty = CRR / ARR
+		// CRRx where 'x' means channel
+
+		TIM4->CCR1 = i;
+		TIM4->CCR2 = 19 - i;
+		osDelay(300);
+	}
+	
+	// osDelay(SENSOR_DELAY);
+    // measure();
+    // osDelay(SENSOR_DELAY);
   }
   /* USER CODE END StartDriveTask */
 }
@@ -49,3 +68,23 @@ void clearReadings() {
 
 
 /* ---- MOTOR FUNCTIONS ---- */
+
+void turnLeft1motor() {
+	HAL_GPIO_WritePin(DRIVR1_GPIO_Port, DRIVR1_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DRIVL1_GPIO_Port, DRIVL1_Pin, GPIO_PIN_SET);
+}
+
+void turnRight1motor() {
+	HAL_GPIO_WritePin(DRIVR1_GPIO_Port, DRIVR1_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(DRIVL1_GPIO_Port, DRIVL1_Pin, GPIO_PIN_RESET);
+}
+
+void turnLeft2motor() {
+	HAL_GPIO_WritePin(DRIVR2_GPIO_Port, DRIVR2_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DRIVL2_GPIO_Port, DRIVL2_Pin, GPIO_PIN_SET);
+}
+
+void turnRight2motor() {
+	HAL_GPIO_WritePin(DRIVR2_GPIO_Port, DRIVR2_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(DRIVL2_GPIO_Port, DRIVL2_Pin, GPIO_PIN_RESET);
+}
